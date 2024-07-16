5703305349e7e4b75b6f3c71cedc4114ebf6e7a7
Assetpho <assetpho@gmail.com>
2024-07-16 11:51:39 +0700

    Update Lao.js

--- Lao.js
+++ Lao.js
@@ -1,3 +1,3 @@
 function setFooter(){
-  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="#" target="_blank"><i class="fa fa-car" style="font-size:24px;"></i></i></a></div><center style="font-size:12px;">Xaipanya &copy; 2024 <a style="color:gray;text-decoration:none" href="#" target="_blank"> Contact</a></center>'
+  document.querySelector('.copyright').innerHTML = '<div class="text-center"><a href="#" target="_blank"><i class="fa fa-car"></i></i></i></a></div><center style="font-size:12px;">Xaipanya &copy; 2024 <a style="color:gray;text-decoration:none" href="#" target="_blank"> Contact</a></center>'
   }
